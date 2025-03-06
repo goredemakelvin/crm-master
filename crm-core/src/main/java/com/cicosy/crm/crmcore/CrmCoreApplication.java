ba2cc5c6f01691f7b69be55b20d1376bb50c81ec
Kelvin Goredema <goredemakelvin3@gmail.com>
2025-03-06 16:55:51 +0200

    initial commit

---
+++ crm-core/src/main/java/com/cicosy/crm/crmcore/CrmCoreApplication.java
@@ -0,0 +1,13 @@
+package com.cicosy.crm.crmcore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrmCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrmCoreApplication.class, args);
+    }
+
+}
---
+++ crm-core/src/test/java/com/cicosy/crm/crmcore/CrmCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cicosy.crm.crmcore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrmCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
